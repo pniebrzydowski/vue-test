16b16a7e13836e983638f2884f3504184a356053
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2023-10-01 10:31:49 +0200

    add initial project setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  const count = ref(0)
+</script>
+
+<template>
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
